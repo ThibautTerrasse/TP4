5bd3997d0133ccdabd618869213e85e4261773a7
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-06-12 12:00:48 +0200

    ajout d'un Log

--- app/src/main/java/com/example/admin/tp4/MainActivity.java
+++ app/src/main/java/com/example/admin/tp4/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.admin.tp4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        Log.d("Formation","premier log");
         setContentView(R.layout.activity_main);
     }
 }
